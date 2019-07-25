7dee3dd7bc95584fcea943b4f9e0ec185cb5a03f
w0xter <lpozogilo@gmail.com>
2019-07-25 14:48:14 +0200

    minor changes

--- onettVuePage/components/queriesitem.vue
+++ onettVuePage/components/queriesitem.vue
@@ -83,7 +83,7 @@ export default {
         this.queries = [
             {
                 title:'Display all the stops of the A zone',
-                description:' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut lectus vestibulum, lobortis dui nec, sodales elit. Aliquam malesuada tristique ex, eu rutrum nibh mollis vitae. Aenean posuere convallis turpis, posuere mollis augue gravida a.',
+                description:'These query shows all the subway stops included in the A zone.',
                 html_link:'https://snap.summerofcode.es/sparql?default-graph-uri=http%3A%2F%2Flocalhost%3A8890%2Ftransporte&query=PREFIX+trm%3A+%3Chttps%3A%2F%2Fw3id.org%2Ftransmodel%2Fterms%23%3E%0D%0A%0D%0Aselect+*+where+%7B%0D%0A%0D%0A%23%3Ftrips++trm%3Astop+%3Fid_stop.%0D%0A%3Fid_stop+trm%3ATariffZoneRef+%3FZonaTarif.%0D%0A%3Fid_stop+trm%3AName++%3Fnombre+.%0D%0A%3Fid_stop+trm%3ALatitude+%3Flatitud.%0D%0A%3Fid_stop+trm%3ALongitud+%3Flongitud.%0D%0AFILTER%28%3FZonaTarif+%3D%22A%22%29%0D%0A%7D&format=text%2Fhtml&timeout=0&debug=on&run=+Run+Query+',
                 json_link:'https://snap.summerofcode.es/sparql?default-graph-uri=http%3A%2F%2Flocalhost%3A8890%2Ftransporte&query=PREFIX+trm%3A+%3Chttps%3A%2F%2Fw3id.org%2Ftransmodel%2Fterms%23%3E%0D%0A%0D%0Aselect+*+where+%7B%0D%0A%0D%0A%23%3Ftrips++trm%3Astop+%3Fid_stop.%0D%0A%3Fid_stop+trm%3ATariffZoneRef+%3FZonaTarif.%0D%0A%3Fid_stop+trm%3AName++%3Fnombre+.%0D%0A%3Fid_stop+trm%3ALatitude+%3Flatitud.%0D%0A%3Fid_stop+trm%3ALongitud+%3Flongitud.%0D%0AFILTER%28%3FZonaTarif+%3D%22A%22%29%0D%0A%7D&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on&run=+Run+Query+',
                 query_value:'PREFIX trm: <https://w3id.org/transmodel/terms#>\nselect * where {\n?id_stop trm:TariffZoneRef ?ZonaTarif.\n?id_stop trm:Name  ?nombre .\n?id_stop trm:Latitude ?latitud.\n?id_stop trm:Longitud ?longitud.\nFILTER(?ZonaTarif ="A")\n}',
@@ -91,7 +91,7 @@ export default {
             },
                         {
                 title:'Display all the stops of the line 10',
-                description:' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut lectus vestibulum, lobortis dui nec, sodales elit. Aliquam malesuada tristique ex, eu rutrum nibh mollis vitae. Aenean posuere convallis turpis, posuere mollis augue gravida a.',
+                description:'These query shows all the subway stops of the line 10.',
                 html_link:'https://snap.summerofcode.es/sparql?default-graph-uri=http%3A%2F%2Flocalhost%3A8890%2Ftransporte&query=PREFIX+trm%3A+%3Chttps%3A%2F%2Fw3id.org%2Ftransmodel%2Fterms%23%3E%0D%0Aselect+*+where+%7B%0D%0A%3Fstop_uri+trm%3AName+%3Fstop_name.%0D%0AFILTER%28regex%28str%28%3Fstop_uri%29%2C+%22par_4_%28%5B1%5D%5B8-9%5D%5B7-9%5D%7C%5B1%5D%5B9%5D%5B0-9%5D%7C%5B2%5D%5B0%5D%5B0-5%5D%29%24%22%29%29%0D%0A%7D%0D%0A&format=text%2Fhtml&timeout=0&debug=on&run=+Run+Query+',
                 json_link:'https://snap.summerofcode.es/sparql?default-graph-uri=http%3A%2F%2Flocalhost%3A8890%2Ftransporte&query=PREFIX+trm%3A+%3Chttps%3A%2F%2Fw3id.org%2Ftransmodel%2Fterms%23%3E%0D%0Aselect+*+where+%7B%0D%0A%3Fstop_uri+trm%3AName+%3Fstop_name.%0D%0AFILTER%28regex%28str%28%3Fstop_uri%29%2C+%22par_4_%28%5B1%5D%5B8-9%5D%5B7-9%5D%7C%5B1%5D%5B9%5D%5B0-9%5D%7C%5B2%5D%5B0%5D%5B0-5%5D%29%24%22%29%29%0D%0A%7D%0D%0A&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on&run=+Run+Query+',
                 query_value:'PREFIX trm: <https://w3id.org/transmodel/terms#>\nselect * where {\n?stop_uri trm:Name ?stop_name.\nFILTER(regex(str(?stop_uri), "par_4_([1][8-9][7-9]|[1][9][0-9]|[2][0][0-5])$"))\n}',
@@ -107,7 +107,7 @@ export default {
             },
                                     {
                 title:'Display all the lines of Madrid',
-                description:' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut lectus vestibulum, lobortis dui nec, sodales elit. Aliquam malesuada tristique ex, eu rutrum nibh mollis vitae. Aenean posuere convallis turpis, posuere mollis augue gravida a.',
+                description:'These query show all the subway stops of Madrid.',
                 html_link:'https://snap.summerofcode.es/sparql?default-graph-uri=http%3A%2F%2Flocalhost%3A8890%2Ftransporte&query=PREFIX+trm%3A+%3Chttps%3A%2F%2Fw3id.org%2Ftransmodel%2Fterms%23%3E%0D%0Aselect+*+where+%7B%0D%0A%3Fstop_uri+trm%3AName+%3FStop_name.%0D%0A%3Fstop_uri+trm%3ADescription+%3FStop_address.%0D%0A%0D%0AFILTER%28contains%28str%28%3Fstop_uri%29%2C+%22par%22%29%29%0D%0A%7D%0D%0A&format=text%2Fhtml&timeout=0&debug=on&run=+Run+Query+',
                 json_link:'https://snap.summerofcode.es/sparql?default-graph-uri=http%3A%2F%2Flocalhost%3A8890%2Ftransporte&query=PREFIX+trm%3A+%3Chttps%3A%2F%2Fw3id.org%2Ftransmodel%2Fterms%23%3E%0D%0Aselect+*+where+%7B%0D%0A%3Fstop_uri+trm%3AName+%3FStop_name.%0D%0A%3Fstop_uri+trm%3ADescription+%3FStop_address.%0D%0A%0D%0AFILTER%28contains%28str%28%3Fstop_uri%29%2C+%22par%22%29%29%0D%0A%7D%0D%0A&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on&run=+Run+Query+',
                 query_value:'PREFIX trm: <https://w3id.org/transmodel/terms#>\nselect * where {\n?stop_uri trm:Name ?Stop_name.\n?stop_uri trm:Description ?Stop_address.\nFILTER(contains(str(?stop_uri), "par"))\n}',
